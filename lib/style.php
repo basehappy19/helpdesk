9d99526791dfc16b1cd8598dc693811b920af09f
DESKTOP-EL7LBE9 <pakpoom.td@gmail.com>
2026-04-22 19:02:09 +0700

    favicon

--- lib/style.php
+++ lib/style.php
@@ -1,1 +1,14 @@
 <link rel="stylesheet" href="/style/global.css">
+
+<link rel="icon" href="/style/favicon/favicon.ico" sizes="any">
+<link rel="icon" type="image/png" sizes="32x32" href="/style/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/style/favicon/favicon-16x16.png">
+
+<link rel="apple-touch-icon" sizes="180x180" href="/style/favicon/apple-touch-icon.png">
+
+<link rel="mask-icon" href="/style/favicon/safari-pinned-tab.svg" color="#5bbad5">
+
+<link rel="manifest" href="/style/favicon/site.webmanifest">
+
+<meta name="msapplication-config" content="/style/favicon/browserconfig.xml">
+<meta name="msapplication-TileColor" content="#2d89ef"> <meta name="theme-color" content="#ffffff"> ```
